8b28ca4daf6e7e64dda9a6be146fa5322fc2e00b
DavidAlexanderNowak <david-alexander@gmx.net>
2022-05-04 20:08:34 +0200

    reversemergesort

--- src/de/hska/iwi/ads/solution/sorting/ReverseMergeSort.java
+++ src/de/hska/iwi/ads/solution/sorting/ReverseMergeSort.java
@@ -1,14 +1,23 @@
 package de.hska.iwi.ads.solution.sorting;
 
-public class ReverseMergeSort<E extends Comparable<E>> extends AbstractMergeSortImpl<E> {
+import de.hska.iwi.ads.sorting.AbstractMergesort;
+
+public class ReverseMergeSort<E extends Comparable<E>> extends AbstractMergesort<E> {
 
 	@Override
+	protected void mergesort(E[] a, int left, int right) {
+		if (left < right) {
+			int midIndex = (left + right) / 2;
+			mergesort(a, left, midIndex);
+			mergesort(a, midIndex + 1, right);
+			merge(a, left, midIndex, right);
+		}
+	}
+	
 	protected void merge(E[] a, int left, int midIndex, int right) {
-		// reverse right part
 		ReverseArray<E> reverseArray = new ReverseArray<>();
 		reverseArray.reverse(a, midIndex + 1, right);
 		
-		// merge
 		int l = left;
 		int r = right;
 		for (int i = left; i <= right; i++) {
