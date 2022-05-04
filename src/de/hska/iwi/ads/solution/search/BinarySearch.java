f20fa87b658874a9985f5445bc7ee00a96ea227b
DavidAlexanderNowak <david-alexander@gmx.net>
2022-05-04 19:36:56 +0200

    add comments for presentation of binary search

--- src/de/hska/iwi/ads/solution/search/BinarySearch.java
+++ src/de/hska/iwi/ads/solution/search/BinarySearch.java
@@ -1,97 +1,54 @@
 package de.hska.iwi.ads.solution.search;
 
-import java.util.Arrays;
-
 import de.hska.iwi.ads.search.Search;
 
 public class BinarySearch<E extends Comparable<E>> implements Search<E> {
 
-//	@Override
-//	public int search(E[] array, E key, int left, int right) {
-//
-//		
-//		
-//		// The key is not in array
-//		if (left > right) {
-//			return right + 1;
-//		}
-//		if (right < left) {
-//			return left - 1;
-//		}
-//
-//		// The key is not in array
-////		if (left > right) {
-////			// Case 1. key < array[left]
-////			if (key.compareTo(array[left]) <= -1) {
-////				return left - 1;
-////			}
-////
-////			// Case 2. key > array[right]
-////			if (key.compareTo(array[right]) >= 1) {
-////				return right + 1;
-////			}
-////
-////			// Case 3. array[left] <= key <= array[right]
-////			if (key.compareTo(array[left]) > -1 && key.compareTo(array[right]) < 1) {
-////				// TODO
-////				return Integer.MAX_VALUE / 2;
-////			}
-////		}
-//		// It is not not in the array?
-//
-//		int mid = (left + right) / 2;
-//
-//		// Mid is the searched key
-//		if (key.compareTo(array[mid]) == 0) {
-//			return mid;
-//		}
-//
-//		// The Searched key lies to the left
-//		if (key.compareTo(array[mid]) <= -1) {
-//			return search(array, key, left, mid - 1);
-//		}
-//
-//		// The searched key lies to the right
-//		if (key.compareTo(array[mid]) >= 1) {
-//			return search(array, key, mid + 1, right);
-//		}
-//
-//		// If none of the above match, this is used as an error
-//		return Integer.MAX_VALUE / 4;
-//	}
-
-
-
-		@Override
-		public int search(E[] a, E key, int left, int right) {
-			if (a[left].compareTo(key) > 0) {
-				return left - 1;
-			}
-
-			if (a[right].compareTo(key) < 0) {
-				return right + 1;
-			}
-
-			return simpleSearch(a, key, left, right);
+	@Override
+	public int search(E[] a, E key, int left, int right) {
+		// Key is to the left of the array
+		if (a[left].compareTo(key) > 0) {
+			return left - 1;
 		}
 
-		private int simpleSearch(E[] a, E key, int left, int right) {
-			if (right < left) {
-				return left;
-			}
-			int midIndex = (left + right) / 2;
-			E midValue = a[midIndex];
-
-			if (midValue.compareTo(key) == 0) {
-				if (midIndex - 1 >= 0 && a[midIndex - 1].compareTo(key) == 0) {
-					return simpleSearch(a, key, left, midIndex - 1);
-				}
-				return midIndex;
-			} else if (midValue.compareTo(key) < 0) { // midValue is less than key
-				return simpleSearch(a, key, midIndex + 1, right);
-			} else { // midValue is greater than key
-				return simpleSearch(a, key, left, midIndex - 1);
-			}
+		// Key is to the right of the array
+		if (a[right].compareTo(key) < 0) {
+			return right + 1;
 		}
-	
+
+		return actualSearch(a, key, left, right);
+	}
+
+	private int actualSearch(E[] a, E key, int left, int right) {
+		// Ending condition
+		if (right < left) {
+			return left;
+		}
+
+		// Calculate new mid
+		int midIndex = (left + right) / 2;
+		E midValue = a[midIndex];
+
+		// Middle value is equal to the key
+		if (midValue.compareTo(key) == 0) {
+
+			// Mid is not at the left end of the array yet
+			// AND
+			// The value to the left of the mid is equal to the mid
+			// (to get the leftmost value)
+			if (midIndex - 1 >= 0 && a[midIndex - 1].compareTo(key) == 0) {
+				return actualSearch(a, key, left, midIndex - 1);
+			}
+			return midIndex;
+		}
+		// Middle value is less than key (search the right part of the array)
+		else if (midValue.compareTo(key) < 0) {
+			return actualSearch(a, key, midIndex + 1, right);
+		}
+		// Middle value is greater than key (search the left part of the array)
+		else {
+			return actualSearch(a, key, left, midIndex - 1);
+		}
+	}
+
 }
